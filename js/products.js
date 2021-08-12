736900e7da4eba52571483405a19a24375669974
flashboy2324 <88442285+flashboy2324@users.noreply.github.com>
2021-08-12 14:17:12 -0300

    obligatorio segundo cambio

--- js/products.js
+++ js/products.js
@@ -1,7 +1,7 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-const PRODUCTS_URL = "https://japdevdep.github.io/ecommerce-api/product/all.json";
+const products_url = "https://japdevdep.github.io/ecommerce-api/product/all.json";
 
 
 
